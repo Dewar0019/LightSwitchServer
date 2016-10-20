c069d5afef175cc6d82c7b8da7b0986cf452ee69
Dewar0019 <Dewar0019@gmail.com>
2016-10-19 21:43:10 -0400

    setup mongodb

--- main.js
+++ main.js
@@ -1,21 +1,17 @@
-var currentPage = null;
 
-// $( document ).ready(function() {
-//     if(currentPage == null) {
-//     	currentPage = "volen";
-//     	$( "#volen" ).addClass("selected");
-//     }
-// });
 
-// $("#makerlab").click(function(event) {
-//         alert(event.target.id);
-// });
+var mongodb = require('mongodb');
 var port = process.env.PORT || 8080;
+var dbuser = process.env.USER;
+var dbpassword = process.env.PASS;
 var express = require('express');
 var app = express();
 app.use(express.static(__dirname + '/public'));
 
 
+var MongoClient = mongodb.MongoClient;
+
+var url = 'mongodb://'+dbuser+':'+dbpassword+'@ds027415.mlab.com:27415/heroku_tpwqr7r3';
 
 
 app.get('/', function(req, res) {
@@ -31,6 +27,21 @@ var server = app.listen(port, function() {
    var host = server.address().address
    var port = server.address().port
 });
+
+MongoClient.connect(url, function (err, db) {
+  if (err) {
+    console.log('Unable to connect to the mongoDB server. Error:', err);
+  } else {
+    //HURRAY!! We are connected. :)
+    console.log('Connection established to', url);
+
+    // do some work here with the database.
+
+    //Close connection
+    db.close();
+  }
+});
+
 
 
 
