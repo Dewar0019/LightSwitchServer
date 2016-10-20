054863812dc200051976abe6a0fb397ff415bf21
Dewar0019 <Dewar0019@gmail.com>
2016-10-19 23:33:25 -0400

    try new mongodb connector

--- main.js
+++ main.js
@@ -1,14 +1,13 @@
 
 
-var mongodb = require('mongodb');
+var mongoose = require ("mongoose"); 
 var port = process.env.PORT || 8080;
 
 var express = require('express');
 var app = express();
 app.use(express.static(__dirname + '/public'));
 
-var MongoClient = mongodb.MongoClient;
-var url = process.env.MONGOLAB_URI
+var uristring = process.env.MONGOLAB_URI
 
 
 
@@ -26,19 +25,13 @@ var server = app.listen(port, function() {
    var port = server.address().port
 });
 
-MongoClient.connect(url, function (err, db) {
-  if (err) {
-    console.log('Unable to connect to the mongoDB server. Error:', err);
-  } else {
-    //HURRAY!! We are connected. :)
-    console.log('Connection established to', url);
-
-    // do some work here with the database.
-
-    //Close connection
-    db.close();
-  }
-});
+mongoose.connect(uristring, function (err, res) {
+      if (err) {
+      console.log ('ERROR connecting to: ' + uristring + '. ' + err);
+      } else {
+      console.log ('Succeeded connected to: ' + uristring);
+      }
+    });
 
 
 
