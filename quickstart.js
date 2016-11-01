7f2c76aab52c2eec43f363392c1d0c78d4675902
Dewar Tan <dewar.tan@mobileheartbeat.com>
2016-11-01 17:04:14 -0400

    found tutorial on stack overflow

--- quickstart.js
+++ quickstart.js
@@ -1,54 +1,14 @@
 var google = require('googleapis');
-var OAuth2 = google.auth.OAuth2;
-var fs = require('fs');
-var key = JSON.parse(fs.readFileSync('key.json', 'utf8')).installed;
-
-// console.log(key);
-var oauth2Client = new OAuth2(
-  key.client_id,
-  key.client_secret,
-  key.redirect_uris
-);
-
-// generate a url that asks permissions for Google+ and Google Calendar scopes
-var scopes = [
-  'https://www.googleapis.com/auth/plus.me',
-  'https://www.googleapis.com/auth/calendar'
-];
-
-google.options({
-  auth: oauth2Client
-});
+var googleCal = googleapis.calendar("v3");
+var serviceEmail = "********@developer.gserviceaccount.com",
+var serviceKeyFile = "./key.pem";
 
 
 
-function listEvents(auth) {
-  
-  var calendar = google.calendar('v3');
-  calendar.events.list({
-    auth: auth,
-    calendarId: 'primary',
-    timeMin: (new Date()).toISOString(),
-    maxResults: 10,
-    singleEvents: true,
-    orderBy: 'startTime'
-  }, function(err, response) {
-    if (err) {
-      console.log('The API returned an error: ' + err);
-      return;
-    }
-    var events = response.items;
-    if (events.length == 0) {
-      console.log('No upcoming events found.');
-    } else {
-      console.log('Upcoming 10 events:');
-      for (var i = 0; i < events.length; i++) {
-        var event = events[i];
-        var start = event.start.dateTime || event.start.date;
-        console.log('%s - %s', start, event.summary);
-      }
-    }
-  });
-}
 
-listEvents(oauth2Client);
+var authClient = new googleapis.auth.JWT(
+        serviceEmail,
+        serviceKeyFile,
+        null,
+        ["https://www.googleapis.com/auth/calendar"]
+    );
